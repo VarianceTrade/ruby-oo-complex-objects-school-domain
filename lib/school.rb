de8d18c79196ac5ed7f682138b02d969c4339b9e
Brandon <sugb73@gmail.com>
2021-01-31 20:35:37 -0500

    commit tree 808d1fcb18d4bd3e92a704a8f914bc8893030b4d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,68 @@
 # code here!
+class School
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student_name, grade)
+        if !roster[grade]
+            roster[grade] = []
+        end
+        roster[grade].push(student_name)
+    end
+
+    def grade(number)
+        return roster[number]
+    end
+
+    def sort
+        roster.each do |grade, students|
+            students.sort!
+        end
+    end
+end
+
+# roster = { 
+#     9 => ["Zach", "Sam", "Pam"], 
+#     10 => ["Sally", "John", "Ted"]
+# }
+
+
+# school.add_student("John", 9)
+# school.add_student("Sam", 10)
+
+# roster = {}
+
+# roster = {9 => ["Zach", "Sam"], 10 => ["Sally", "John"]}
+
+# roster[9].push("Pam")
+
+# roster = { 9 => ["Zach", "Sam", "Pam"], 10 => ["Sally", "John"] }
+
+# roster[10].push("Ted")
+
+
+
+# if (!roster.include? 11) {
+#     roster[11] = []
+# }
+
+
+# roster = { 
+#     9 => ["Zach", "Sam", "Pam"], 
+#     10 => ["Sally", "John", "Ted"]
+#     11 => []
+# }
+
+# roster[11].push("Tammy")
+
+# roster = { 
+#     9 => ["Zach", "Sam", "Pam"], 
+#     10 => ["Sally", "John", "Ted"],
+#     11 => ["Tammy"]
+# }
